99181a84c9f842b1f095ef88c172bd708b23ab36
Bryan Koh <s10223617@connect.np.edu.sg>
2022-02-02 02:48:43 +0800

    Improved index.js

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {   
     $("#Login-form h2").click(function(e) {
         if ($("#Login-form h2").css("margin-bottom") == "60px") {
+            $("#Login-form").css("margin-bottom", "100px");
             $("#Login").slideUp("fast", function(ee) {
                 $("#Login-form h2").css({"margin-bottom": "0px"});
             });
         }
         else {
             $("#Login-form h2").css("margin-bottom", "60px");
+            $("#Login-form").css("margin-bottom", "40px");
             $("#Login").slideDown("fast", function(ee) {              
-                $("#Login").css("display", "block");            
+                $("#Login").css("display", "block");                           
             });
         }
     }); 
