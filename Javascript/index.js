cfc9748982e12add1f270c976f86c9f5d6db8293
Bryan Koh <s10223617@connect.np.edu.sg>
2022-02-02 01:52:39 +0800

    Fixed mobile display for index.html

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function() {   
     $("#Login-form h2").click(function(e) {
         if ($("#Login-form h2").css("margin-bottom") == "60px") {
             $("#Login").slideUp("fast", function(ee) {
