c97cb2ba33737150e185b0bd35029e6334915ef9
Bryan Koh <s10223617@connect.np.edu.sg>
2022-02-14 13:16:57 +0800

    fixed a bug with profile picture change

--- Javascript/main-menu.js
+++ Javascript/main-menu.js
@@ -57,38 +57,38 @@ $(document).ready(function () {
                 }
                 $.ajax(settings).done(function (response) {
                     console.log(response);
-                });
-                localStorage.removeItem('user1');
-                let settings1 = {
-                    "async": true,
-                    "crossDomain": true,
-                    "url": "https://learnpythonatnp-b99e.restdb.io/rest/student",
-                    "method": "GET",
-                    "headers": {
-                      "content-type": "application/json",
-                      "x-apikey": APIKEY,
-                      "cache-control": "no-cache"
-                    },
-                }
-                $.ajax(settings1).done(function (response1) {
-                    console.log(response1);
-                    for (let i = 0; i < response1.length; i++) {
-                        if (response1[i].username == userinfo.username) {
-                            localStorage.setItem('user1', JSON.stringify(response1[i]));
-                        }
+                    localStorage.removeItem('user1');
+                    let settings1 = {
+                        "async": true,
+                        "crossDomain": true,
+                        "url": "https://learnpythonatnp-b99e.restdb.io/rest/student",
+                        "method": "GET",
+                        "headers": {
+                        "content-type": "application/json",
+                        "x-apikey": APIKEY,
+                        "cache-control": "no-cache"
+                        },
                     }
-                })
-                Swal.fire({
-                    title: 'Your profile picture has been changed!',
-                    text: 'The page will reload. Please wait so that the changes are reflected.',
-                    icon: 'success',
-                    timer: 6000,
-                    timerProgressBar: true,
-                    showConfirmButton: false
-                })
-                .then(function () {
-                    window.location.replace("main-menu.html");
-                })
+                    $.ajax(settings1).done(function (response1) {
+                        console.log(response1);
+                        for (let i = 0; i < response1.length; i++) {
+                            if (response1[i].username == userinfo.username) {
+                                localStorage.setItem('user1', JSON.stringify(response1[i]));
+                            }
+                        }
+                    })
+                    Swal.fire({
+                        title: 'Your profile picture has been changed!',
+                        text: 'The page will reload. Please wait so that the changes are reflected. If changes are not reflected, re-login again and the profile picture will be updated.',
+                        icon: 'success',
+                        timer: 7000,
+                        timerProgressBar: true,
+                        showConfirmButton: false
+                    })
+                    .then(function () {
+                        window.location.replace("main-menu.html");
+                    })
+                });
             } else if (answer.dismiss === Swal.DismissReason.cancel) {
                 if (number === 9) {
                     number = 0;
